4120aed646458eb9cdc5cba97e2ba63136294cd5
eysu <jamieralcheon30@gmail.com>
2026-07-22 18:07:30 +0800

    Fix supabase url format

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
 export const supabase = createClient(
-  'https://vgtvobuxwdzkxnzdzfwp.supabase.co/rest/v1/',
+  'https://vgtvobuxwdzkxnzdzfwp.supabase.co',
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZndHZvYnV4d2R6a3huemR6ZndwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ2Nzk0ODEsImV4cCI6MjEwMDI1NTQ4MX0.RNyIYL4K8yvT0dnlcer-EdATLTpOlACyCKdA9igdTxU'
 );
  
